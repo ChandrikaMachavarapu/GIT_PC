d8ad9b14d41de547e7477974f1644e18aef4ad79
unknown <chandrikaus97@gmail.com>
2021-12-09 14:59:58 -0500

    new file added

---
+++ p1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 22:21:51 2020
+
+@author: chand
+"""
+
+print("Hello I am First Assignment")
+print("Next Week more Python is coming")
+print("and it is lot's of fun")
+
+
+
+Height=int(input("height: "))
+Weight=int(input("weight: "))
+BMI=(Weight*703)/(Height*Height)
+print(BMI)
+
+
+
+
+name=input(" please enter your name")
+if(name=="ying"):
+    print("Hello Ying")
+else:
+    print("I don't know you, but welcome.")
+    
